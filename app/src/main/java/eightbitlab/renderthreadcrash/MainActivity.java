1bb1c168912eb97e0434088a82587459beee9149
saviukd <Dmytro.Saviuk@ig.com>
2018-06-13 16:59:36 +0200

    Simplify setup

--- app/src/main/java/eightbitlab/renderthreadcrash/MainActivity.java
+++ app/src/main/java/eightbitlab/renderthreadcrash/MainActivity.java
@@ -1,26 +1,24 @@
 package eightbitlab.renderthreadcrash;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AlertDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new LolDialogFragment().show(getSupportFragmentManager(), "TAG");
+        dialog = new AlertDialog.Builder(this).setView(R.layout.layout_with_edit_text).create();
+        dialog.show();
     }
 
-    public static class LolDialogFragment extends DialogFragment {
-        @Override
-        @NonNull
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new AlertDialog.Builder(getActivity()).setView(R.layout.layout_with_edit_text).create();
-        }
+    @Override
+    protected void onDestroy() {
+        dialog.dismiss();
+        super.onDestroy();
     }
 }
